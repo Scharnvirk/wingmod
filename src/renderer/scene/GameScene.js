3a831f106a95c98c75b72515b9f723955ecf2dbf
Jarek Ksiazczyk <jarek.ksiazczyk@gmail.com>
2016-01-22 21:40:14 +0100

    mini swiatla

--- src/renderer/scene/GameScene.js
+++ src/renderer/scene/GameScene.js
@@ -56,8 +56,8 @@ class GameScene {
         this.emergencyLight.position.set( 0, 0, 50 );
         this.scene.add( this.emergencyLight );
 
-        for (let i = 0; i < 5; i++){
-            let l =  new THREE.PointLight( 0xffffff, 1, 100 );
+        for (let i = 0; i < 15; i++){
+            let l =  new THREE.PointLight( 0xffffff, 1, 1 );
             l.position.set(Utils.rand(-200,200), Utils.rand(-200,200), 20);
             this.scene.add(l);
         }
