166c231d0bb552c9a788512dd3be352301d52797
Jarek Ksiazczyk <jarek.ksiazczyk@gmail.com>
2016-02-08 00:48:39 +0100

    fireforks

--- src/renderer/actor/ProjectileActor.js
+++ src/renderer/actor/ProjectileActor.js
@@ -1,11 +1,15 @@
 function ProjectileActor(){
     BaseActor.apply(this, arguments);
+    this.colorR = Math.random();
+    this.colorG = Math.random();
+    this.colorB = Math.random();
 }
 
 ProjectileActor.extend(BaseActor);
 
 ProjectileActor.prototype.customUpdate = function(){
     this.particleManager.createParticle('particleAdd',[this.position[0], this.position[1], 1,1,1, 5, 0.5, 50]);
+    this.particleManager.createParticle('particleAdd',[this.position[0], this.position[1], this.colorR, this.colorG, this.colorB, 20, 0.1, 2]);
 };
 
 //
