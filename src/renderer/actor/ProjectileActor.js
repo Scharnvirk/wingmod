13d612d0e56081716b8ab0cf1877a8c8fd99d21e
Jarek Ksiazczyk <jarek.ksiazczyk@gmail.com>
2016-02-10 00:58:44 +0100

    jednak szarsze dymki

--- src/renderer/actor/ProjectileActor.js
+++ src/renderer/actor/ProjectileActor.js
@@ -14,7 +14,7 @@ ProjectileActor.prototype.customUpdate = function(){
 
 ProjectileActor.prototype.onDeath = function(){
     for (let i = 0; i < 10; i++){
-        this.particleManager.createParticle('smokePuffAlpha',this.position[0] + Utils.rand(-5,5), this.position[1] + Utils.rand(-5,5), this.colorR, this.colorG, this.colorB, Utils.rand(5,20), 0.2, 60);
+        this.particleManager.createParticle('smokePuffAlpha',this.position[0] + Utils.rand(-5,5), this.position[1] + Utils.rand(-5,5), this.colorR*0.3+0.7, this.colorG*0.3+0.7, this.colorB*0.3+0.7, Utils.rand(5,20), 0.2, 60);
     }
     this.particleManager.createParticle('particleAdd', this.position[0], this.position[1], this.colorR, this.colorG, this.colorB, 80, 0.3, 2);
     this.particleManager.createParticle('particleAdd', this.position[0], this.position[1], this.colorR, this.colorG, this.colorB, 20, 1, 20);
