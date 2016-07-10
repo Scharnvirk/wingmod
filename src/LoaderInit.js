b22f4cd8ba401ece2e5ae1a4853d676e01020ada
Jarek Ksiazczyk <jarek.ksiazczyk@gmail.com>
2016-07-10 19:08:30 +0200

    cleanup

--- src/LoaderInit.js
+++
@@ -1,8 +0,0 @@
-global.Utils = require("shared/Utils");
-global.Constants = require("shared/Constants");
-global.EventEmitter = require("shared/EventEmitter");
-
-if('function'===typeof importScripts){
-    var LoaderCore = require('loader/Core');
-    self.core = new LoaderCore(self);
-}
